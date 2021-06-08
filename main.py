6707c7f48919c2848e9b8d10cdcb6223e2356050
Daniel Davis <dda49@uclive.ac.nz>
2021-06-09 02:08:51 +1200

    Code ready for submission

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-__version__ = "0.0.2"
-
 import cv2
 import numpy as np
 
 import kivy
-#kivy.require('2.0.0')
 
 from kivy.app import App
-from kivy.uix.button import Button
-
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-
-from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.graphics.texture import Texture
 from kivy.graphics import Rectangle
-from kivy.uix.image import Image
 from kivy.properties import *
 
+# Set the following variables if the aim is to collect training images for a learning algorithm.
+# Only a single dice should be in frame at a time and only focussed on one face value.
+
+RECORD_TRAINING_IMAGES = False # Bool value which chooses betweening recording training images and classification
+DICE_TYPE = 'd6'               # The type of dice, only used for recording images
+DICE_FACE = '1'                # The face of the dice when recording images
 
 class CameraWidget(Widget):
     
     img_frame = ObjectProperty(force_dispatch=True)
     
-    def __init__(self, record_training_images=False, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        self.record_training_images = record_training_images
-        
+        # Init class info
         self.cap = cv2.VideoCapture('./10s_clip.avi')
-        _, old_frame = self.cap.read()
-        # Preprocess image
-        blur = cv2.GaussianBlur(old_frame, (9,9), 0)
-        self.old_grey_image = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        
-        ret, frame = self.cap.read()
-        
         self.dice_count = None
         self.dice_position = np.ones((0, 3), dtype='float32')
         
-        if self.record_training_images:
-            dice_type = 'd6'#f'image_folder_{count}'
-            face = '6'
+        # Read one image frame for setup purposes
+        ret, frame = self.cap.read()
+        
+        if not ret:
+            raise Exception("No frame returned by VideoCapture, capture device likely incorrect.")
+        
+        # Preprocess image
+        blur = cv2.GaussianBlur(frame, (9,9), 0)
+        self.old_grey_image = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)        
+        
+        if RECORD_TRAINING_IMAGES:
+            # Setup info for training image collection
+            dice_type = DICE_TYPE
+            face = DICE_FACE
             self.path = f"{dice_type}\\{face}"
             
             try:
@@ -57,6 +58,7 @@ class CameraWidget(Widget):
             
             self.image_count = max([int(x.removesuffix('.jpg')) for x in os.listdir(self.path)]) + 1
         else:
+            # Setup up pretrained TensorFlow classification net in OpenCV
             self.net = cv2.dnn.readNetFromTensorflow(f'frozen_models\\frozen_graph.pb')
         
         # Init Kivy info
@@ -64,36 +66,41 @@ class CameraWidget(Widget):
         self.screen_height = frame.shape[0]
         self.texture_array = Texture.create(size=(self.screen_width, self.screen_height))
         Window.size = (self.screen_width, self.screen_height)
-            
     
     
     def capture_frame(self, *_):
+        """ This method is scheduled by the widget to run at a fixed rate and updates the captured images
+        """
         
         ret, frame = self.cap.read()  # Read an image from the frame.
         if ret:
             
-            frame = self.process(frame)
+            frame = self.process(frame) # Runs the computer vision algorithms
             
+            # Convert image to format for widget to display
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.flip(frame, 0)
+            self.img_frame = cv2.flip(frame, 0)
                 
-            self.img_frame = self.mute_channels(frame, '')
-                    
-    
+            
     def on_img_frame(self, *_):
+        """ This method is triggered by self.img_frame changing and displays it on the widget
+        """
         
         arr = self.img_frame.reshape((-1))
         arr = arr.astype('ubyte')
         
-        # now blit the array
+        # Blit the array onto a Kivy texture
         self.texture_array.blit_buffer(arr, colorfmt='rgb', bufferfmt='ubyte')      
         
+        # Print the texture to the widget canvas
         with self.canvas:
             Rectangle(texture=self.texture_array, pos=(0, 0), 
                       size=(self.screen_width, self.screen_height))
             
             
     def close_camera(self):
+        """ Release the capture device on application close
+        """
         self.cap.release()
 
 
@@ -129,6 +136,7 @@ class CameraWidget(Widget):
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
                     100, 1.0)
         
+        # Run k-means algorithm on coordinates of 'dice' pixels
         compactness, labels, centres = cv2.kmeans(data=pixelpoints,
                                                   K=K,
                                                   bestLabels=bestLabels,
@@ -137,7 +145,7 @@ class CameraWidget(Widget):
                                                   flags=flags,
                                                   centers=centres)
         
-        assert centres.shape[1] == 2
+        assert centres.shape[1] == 2 # Testing that at least one 'dice' pixel was input
         
         return centres
     
@@ -146,13 +154,15 @@ class CameraWidget(Widget):
         """ Using the dice centres from the kalman posterior values and either a set of cluster labels or a
             thresholded image, find the convex hulls for each dice.
         """
+        # If no dice are in the image, return nothing
         if self.dice_position.size == 0 or pixelpoints.size == 0:
             return {}
         
+        # Choose the most likely dice centres
         centres = self.dice_position[:int(round(self.dice_count)), :2].astype(np.float32)
         
         if labels is None:
-            
+            # Using k nearest neighbours, assign each 'dice' pixel to a dice centre
             knn = cv2.ml.KNearest_create()
             
             responses = np.arange(centres.shape[0])
@@ -166,16 +176,21 @@ class CameraWidget(Widget):
         
         hull_dict = {i: None for i in range(centres.shape[0])}
         for i in range(centres.shape[0]):
-            # The locations of the pixels in group i
+            # The locations of the 'dice' pixels in dice group i
             tmp = pixelpoints[np.nonzero(labels == i)[0], :]
             
+            # Create a mask for dice i
             mask = np.zeros((frame_size[0], frame_size[1]), dtype=np.uint8)
-            # Set all pixels in group i to white
+            
+            # Set all pixels in dice group i to white
             mask[tmp[:, 0], tmp[:, 1]] = 255
             
+            # Find the contour which outlines die i
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
             
             if contours:
+                # Join the contours together to merge distoint pieces of the same die
+                # Find the convex hull of the resultant contour
                 hull_dict[i] = cv2.convexHull(np.concatenate(contours))
             else:
                 del hull_dict[i]
@@ -199,9 +214,11 @@ class CameraWidget(Widget):
             # Update dice positions with motion model guess
             dice_centres_new, st, err = cv2.calcOpticalFlowPyrLK(self.old_grey_image, grey_image, dice_centres, None, **lk_params)
             self.dice_position[:, 0:2] = np.flip(dice_centres_new, 1)
-            
+        
+        # Add motion model variance
         self.dice_position[:, 2] *= 1.1
         
+        # Update reference image
         self.old_grey_image = grey_image.copy()
     
     
@@ -209,13 +226,15 @@ class CameraWidget(Widget):
         """ Update the Kalman filters with the position estimations from the kmeans clustering.
         """
         
+        # Skip update step if the k-means algorithm returned no dice centres
         if sensors.size == 0:
             self.dice_position[:, 2] *= 1.1
             return
         
+        
+        # Run kNN algorithm to match sensor points found by k-means to known dice centres
         knn = cv2.ml.KNearest_create()
         
-        # Run kNN algorithm to match sensor points to known points
         trainData = self.dice_position[:, 0:2]
         responses = np.arange(trainData.shape[0])
         
@@ -223,37 +242,50 @@ class CameraWidget(Widget):
         ret, results, neighbours, dist = knn.findNearest(sensors, 1)
         results = results.astype('uint8')
         
+        # Init Kalman algorithm values
         initial_variance = 3
         sensor_var = 0.9
         variance_gain = 1.3
+        max_distance = 100
+        max_var = 5
         
         # If no matching point from sensor, lower weight
         for point_i in set(responses).difference(set(results.flatten())):
             self.dice_position[point_i, 2] *= variance_gain
         
         for i, (x, y) in enumerate(sensors):
-            max_distance = 100
             if dist[i] > max_distance:
-                # Add new possible point
+                # Add new possible point if nearest matching centre is too far away
                 new_point = np.array([x, y, initial_variance]).reshape((1, 3)).astype('float32')
                 self.dice_position = np.append(self.dice_position, new_point, axis=0)
                 
             else:
+                # Otherwise, fuse new and old dice points, updating position and variance
                 response_i = results[i, 0]
-                nearest_point = self.dice_position[response_i]
-                K = nearest_point[2] / (sensor_var + nearest_point[2])
-                self.dice_position[response_i, :2] = K * sensors[i] + (1 - K) * self.dice_position[response_i, :2]
-                self.dice_position[response_i, 2] = sensor_var * K
-            
+                nearest_point, nearest_point_var = self.dice_position[response_i, :2], self.dice_position[response_i, 2]
+                
+                K = nearest_point_var / (sensor_var + nearest_point_var) # Kalman gain
+                updated_position = K * sensors[i] + (1 - K) * nearest_point
+                updated_variance = sensor_var * K
+                
+                self.dice_position[response_i, :2] = updated_position
+                self.dice_position[response_i, 2] = updated_variance
+        
+        # Delete any dice centres with variance too high
         for i in range(self.dice_position.shape[0] - 1, -1, -1):
-            max_var = 5
             if self.dice_position[i][2] > max_var:
                 self.dice_position = np.delete(self.dice_position, i, axis=0)
-                
+        
+        # Sort dice centres by ascending variance
         self.dice_position = self.dice_position[self.dice_position[:, 2].argsort()]
     
     
     def extract_dice_image(self, frame, hull, size=(50, 50)):
+        """ Given an image frame and a die convex hull, return a minimum sized square image containing the die hull
+            Return square's centre in frame and image
+        """
+        
+        # Find the minimum sized square which contains the hull
         rect = cv2.minAreaRect(hull)
         rect = (rect[0], (max(rect[1]), max(rect[1])), rect[2])
         
@@ -281,6 +313,8 @@ class CameraWidget(Widget):
         
     
     def find_blobs(self, im):
+        """ Return a blob detector for approximate dice objects in frame
+        """
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
         
@@ -297,54 +331,75 @@ class CameraWidget(Widget):
     
     
     def find_dice_count(self, grey_image, thresh):
+        """ Estimate the number of dice in frame and update a Kalman filter to track value
+        """
+        
+        # Find the total area occupied by dice in the frame
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         total_area = sum([cv2.contourArea(cnt) for cnt in contours])
         
         adjustment_factor = 0.85 # Compensate for the tendency for the contours to give lower
                                  #   areas than the circular blobs
         areas = [(kp.size/2)**2 * np.pi for kp in self.find_blobs(grey_image)]
+        
+        # Find the estimated average dice size
         if areas:
             average_dice_size = np.average(areas) * adjustment_factor
         else:
-            average_dice_size = total_area
+            average_dice_size = total_area # If not blobs are found estimate a single die
             
         average_dice_size = min(average_dice_size, # Prevent dice number edge cases:
                                 total_area)        #   Impossible to have between 0 and 1 dice
         
-        new_dice_count = total_area / max(1, average_dice_size)
+        # Estimate number of dice
+        try:
+            new_dice_count = total_area / average_dice_size
+        except ZeroDivisionError:
+            new_dice_count = total_area
+        
+        
         if self.dice_count is None:
             self.dice_count = new_dice_count
         else:
+            # Update Kalman filter for number of dice. Uses an experimental equation for Kalman gain to
+            #     speed up change when estimate is far from old value
             K = min(0.1 + 0.1 * abs(new_dice_count - self.dice_count), 0.5)
             self.dice_count = K * new_dice_count + (1 - K) * self.dice_count
         
     
-    def process(self, frame):        
+    def process(self, frame):    
+        """ The main algorithm process. Does basic image processing and calls helper functions.
+        """    
         image_processed = frame.copy()
         
         # Preprocess image
         blur = cv2.GaussianBlur(frame, (9,9), 0)
-        
-        # convert the image to grayscale
         grey_image = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
         
+        # Threshold image and perform morphogical closing.
         ret, thresh = cv2.threshold(grey_image, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
         kernel = np.ones((11, 11), dtype='uint8')
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
         
+        # Find the coordinates of pixels belonging to dice
         pixelpoints = np.transpose(np.nonzero(thresh)).astype(np.float32)
-            
+        
+        # Update the estimated number of dice in frame
         self.find_dice_count(grey_image, thresh)
         
+        # Estimate the centres of dice in the frame
         centres = self.k_means_dice(pixelpoints)
         
         if self.dice_position.shape[0] == 0:
             self.dice_position = np.concatenate((centres, np.ones((centres.shape[0], 1))), axis=1).astype('float32')
         
+        # Update the positions of the previously estimated dice centres
         self.motion_model(grey_image)
         
+        # Update dice centre kalman filters
         self.kalman_update(centres)
         
+        # Find the convex hulls which give the shape of each dice
         hull_dict = self.find_dice_hulls(pixelpoints, frame.shape)
         
         for _, hull in hull_dict.items():
@@ -355,65 +410,33 @@ class CameraWidget(Widget):
             green = (0, 255, 0)
             cv2.drawContours(image_processed,[hull_bbox],0,green,2)
             
-            if self.record_training_images:
+            if RECORD_TRAINING_IMAGES:
+                # Save image of dice as a training image
                 cv2.imwrite(f'{self.path}\\{self.image_count}.jpg', dice_image)
                 self.image_count += 1
             else:
-                # Predict outcomes
+                # Perform classification of each dice using the TensorFlow net
                 blob = cv2.dnn.blobFromImage(cv2.cvtColor(dice_image, cv2.COLOR_BGR2GRAY), 1, (50, 50))
                 
                 self.net.setInput(blob)
                 preds = self.net.forward()
-                
                 pred_classes = np.argsort(preds[0])[::-1] + 1
                 
-                # font
                 font = cv2.FONT_HERSHEY_SIMPLEX
-                # fontScale
                 fontScale = 1            
                 
                 image_processed = cv2.putText(image_processed, str(pred_classes[0]), hull_centre, font, 
-                                              fontScale, green, thickness=3)            
+                                              fontScale, green, thickness=3)
                 
-            
-        #image_processed = cv2.putText(image_processed, str(self.dice_count), (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 
-                                      #1, (0, 255, 0), thickness=2)    
         return image_processed    
-    
-    def gray_to_rgb_format(self, frame):
-        new_frame = np.zeros((self.screen_height, self.screen_width, 3), dtype=int)
-        old_frame = np.expand_dims(frame, axis=2)
-        return new_frame + old_frame
-    
-    
-    def mute_channels(self, frame, channels):
-        if channels == '':
-            return frame
-        
-        channel_index = {
-            'r': 0,
-            'g': 1,
-            'b': 2
-        }
-        channel_selector = np.ones(3, dtype=int)
-        for channel in channels:
-            channel_selector[channel_index[channel]] = 0
-        
-        mask = np.zeros(frame.shape, dtype=int) + channel_selector
-           
-        return frame * mask
 
 
 class MainApp(App):
     
     def build(self):
-        #return Button(text='hello world')
-        #cap = cv2.VideoCapture(0)  # Open the first camera connected to the computer.
-        #ret, frame = cap.read()  # Read an image from the frame.   
-        
-        #return Button(text=f'{len(frame)} x {len(frame[0])}')        
         self.widget_inst = CameraWidget()
         
+        # schedule app to run at 10 FPS
         Clock.schedule_interval(self.widget_inst.capture_frame, 1/10)
         
         return self.widget_inst
@@ -425,4 +448,4 @@ class MainApp(App):
 if __name__ == "__main__":
     app_inst = MainApp()
     app_inst.run()
-    #app_inst.close_app()
+    app_inst.close_app()
--- tf_train_model.py
+++ tf_train_model.py
@@ -39,12 +39,6 @@ test_labels = all_y[index[train_N:]]
 
 class_names = ['1', '2', '3', '4', '5', '6']
 
-#plt.figure()
-#plt.imshow(train_images[0])
-#plt.colorbar()
-#plt.grid(False)
-#plt.show()
-
 # Data Normalization
 # Conversion to float
 train_images = train_images.astype(np.float32) 
@@ -54,15 +48,6 @@ test_images = test_images.astype(np.float32)
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-#plt.figure(figsize=(10,10))
-#for i in range(25):
-    #plt.subplot(5,5,i+1)
-    #plt.xticks([])
-    #plt.yticks([])
-    #plt.grid(False)
-    #plt.imshow(train_images[i], cmap=plt.cm.binary)
-    #plt.xlabel(class_names[train_labels[i]])
-#plt.show()
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(50, 50)),
@@ -112,85 +97,3 @@ tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
                   logdir="./frozen_models",
                   name="frozen_graph.pb",
                   as_text=False)
-
-
-"""
-def plot_image(i, predictions_array, true_label, img):
-    true_label, img = true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.imshow(img, cmap=plt.cm.binary)
-
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                       100*np.max(predictions_array),
-                                class_names[true_label]),
-             color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-    true_label = true_label[i]
-    plt.grid(False)
-    plt.xticks(range(1, 7))
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
-
-# Plot the first X test images, their predicted labels, and the true labels.
-# Color correct predictions in blue and incorrect predictions in red.
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-    plt.subplot(num_rows, 2*num_cols, 2*i+1)
-    plot_image(i, predictions[i], test_labels, test_images)
-    plt.subplot(num_rows, 2*num_cols, 2*i+2)
-    plot_value_array(i, predictions[i], test_labels)
-plt.tight_layout()
-plt.show()
-"""
-
-'''
-# load the input image from disk
-image = test_images[0, :, :].reshape((50, 50))
-
-blob = cv2.dnn.blobFromImage(image, 1, (50, 50))
-
-net = cv2.dnn.readNetFromTensorflow(f'frozen_models\\frozen_graph.pb')
-
-net.setInput(blob)
-start = time.time()
-preds = net.forward()
-end = time.time()
-print("[INFO] classification took {:.5} seconds".format(end - start))
-
-# sort the indexes of the probabilities in descending order (higher
-# probabilitiy first) and grab the top-5 predictions
-idxs = np.argsort(preds[0])[::-1]
-
-# loop over the top-5 predictions and display them
-for (i, idx) in enumerate(idxs):
-    # draw the top prediction on the input image
-    if i == 0:
-        text = "Label: {}, {:.2f}%".format(class_names[idx],
-                                                   preds[0][idx] * 100)
-        cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-                            0.7, (0, 0, 255), 2)
-    # display the predicted label + associated probability to the
-    # console	
-    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
-                                                            class_names[idx], preds[0][idx]))
-# display the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)'''
